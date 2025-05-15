6c15bfa03007d7bda03185ad3b667e460fe1f769
jltato <84922122+jltato@users.noreply.github.com>
2025-05-14 23:22:20 -0300

    feat paginacion, dataTables_ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'listado/:tipo',
+    loadComponent: () =>
+      import('./list/list.component').then(m => m.ListComponent)
+  },
+  {
+    path: 'gestion/:id',
+    loadComponent: () =>
+      import('./gestion/gestion.component').then(g => g.GestionComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full' // Ruta por defecto
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  }
+];
